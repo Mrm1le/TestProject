d4f2d86a7ddf8cd6af43f88681ad10456a15ad36
Tian Haijian <13086837042@163.com>
2023-01-03 22:54:18 +0800

    add test string, start test algorithm in vector

--- test_vector/test.cc
+++ test_vector/test.cc
@@ -1,25 +1,30 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <numeric>
+#include <algorithm>
 int main()
 {
     std::vector<std::string> a;
-    std::cout <<  a.size() <<"/" << a.max_size() << std::endl;
-    a.emplace_back("no");
-    std::cout <<  a.size() <<"/" << a.max_size() << std::endl;
-    a.insert(a.begin(), "yes");
-    std::cout <<  a.size() <<"/" << a.max_size() << std::endl;
-    
-    std::vector<std::string> b(10, "nothng");
 
-    b.resize(20, "thing");
+    a.emplace_back("0"); // 插入元素的3种常用方法
+    a.insert(a.begin(), "1"); //push_front()的另类实现
+    std::cout <<  a.size() <<"/" << a.max_size() << std::endl;
+    a.push_back("2");
+    std::cout << "a capacity " << a.capacity() << std::endl;
+    a.reserve(10); //一次性分配可容纳十个元素的内存
+    std::cout << "a capacity " << a.capacity() << std::endl;
+
+    std::vector<std::string> b(20, "nothng"); //一种构造方式
+    b.resize(10, "thing"); //保留旧元素，添加新元素
+    std::cout << "b size " << b.size() << std::endl; // resize()改变元素数量 
+    std::cout << "b capacity " << b.capacity() << std::endl; // resize()不改变容器容量 
     // b.pop_back();
     for(auto i : b) //普通遍历
     {
         std::cout << i << std::endl;
-        // printf("%s\n", i.c_str());
-        b.pop_back();
     }
+    b.pop_back(); //弹出最后一个元素
 
     auto l = a.end();
     auto ll = a.back();
@@ -35,6 +40,13 @@ int main()
     // std::cout << svec[10] << std::endl; // 数组越界检查
     // std::cout << svec.at(10) << std::endl;
 
-    std::cout << "end success" << std::endl;
+    // 常用泛型算法
+    auto id = std::find(a.cbegin(), a.cend(), "1");
+    printf("id [%s] [0x%x]\n", id->c_str(), id); //find查找指定元素
+    auto num = std::count(a.cbegin(), a.cend(), "1"); //count返回个数
+    std::string tmp("join a: ");
+    std::string group = std::accumulate(a.cbegin(), a.cend(), tmp); // 求和函数
+    printf("num [%d]\n", num); //find查找指定元素
+    std::cout << "group " << group << std::endl;
     return 0;
 }
