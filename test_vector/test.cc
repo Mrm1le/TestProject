0db293c552ee743d5d0ab1661f64a0ad8a83e706
Tian Haijian <13086837042@163.com>
2023-01-05 23:53:17 +0800

    add test algo , add lambda function

--- test_vector/test.cc
+++ test_vector/test.cc
@@ -3,50 +3,92 @@
 #include <iostream>
 #include <numeric>
 #include <algorithm>
+
+bool IsShorter(const std::string &s1, const std::string &s2)
+{
+    return s1.size() < s2.size();
+}
 int main()
 {
-    std::vector<std::string> a;
+    std::vector<std::string> svec;
 
-    a.emplace_back("0"); // 插入元素的3种常用方法
-    a.insert(a.begin(), "1"); //push_front()的另类实现
-    std::cout <<  a.size() <<"/" << a.max_size() << std::endl;
-    a.push_back("2");
-    std::cout << "a capacity " << a.capacity() << std::endl;
-    a.reserve(10); //一次性分配可容纳十个元素的内存
-    std::cout << "a capacity " << a.capacity() << std::endl;
+    svec.emplace_back("000");        // 插入元素的3种常用方法
+    svec.insert(svec.begin(), "11"); // push_front()的另类实现
+    std::cout << svec.size() << "/" << svec.max_size() << std::endl;
+    svec.push_back("2");
+    std::cout << "svec capacity " << svec.capacity() << std::endl;
+    svec.reserve(10); // 一次性分配可容纳十个元素的内存
+    std::cout << "svec capacity " << svec.capacity() << std::endl;
 
-    std::vector<std::string> b(20, "nothng"); //一种构造方式
-    b.resize(10, "thing"); //保留旧元素，添加新元素
-    std::cout << "b size " << b.size() << std::endl; // resize()改变元素数量 
-    std::cout << "b capacity " << b.capacity() << std::endl; // resize()不改变容器容量 
+    std::vector<std::string> b(20, "nothng");                // 一种构造方式
+    b.resize(10, "thing");                                   // 保留旧元素，添加新元素
+    std::cout << "b size " << b.size() << std::endl;         // resize()改变元素数量
+    std::cout << "b capacity " << b.capacity() << std::endl; // resize()不改变容器容量
     // b.pop_back();
-    for(auto i : b) //普通遍历
+    for (auto i : b) // 普通遍历
     {
         std::cout << i << std::endl;
     }
-    b.pop_back(); //弹出最后一个元素
+    b.pop_back(); // 弹出最后一个元素
 
-    auto l = a.end();
-    auto ll = a.back();
+    auto l = svec.end();
+    auto ll = svec.back();
     std::cout << *(--l) << " " << ll << std::endl; // 注意end()和back()的区别
 
-    for(auto iter=a.cbegin(); iter!=a.end(); ++iter) // 迭代器遍历
+    for (auto iter = svec.cbegin(); iter != svec.end(); ++iter) // 迭代器遍历
     {
         std::cout << iter->data() << std::endl;
         std::cout << *iter << std::endl;
     }
 
-    std::vector<std::string> svec;
+    // std::vector<std::string> svec;
+    std::vector<int> ivec;
+    std::vector<int> ivec2;
     // std::cout << svec[10] << std::endl; // 数组越界检查
     // std::cout << svec.at(10) << std::endl;
 
     // 常用泛型算法
-    auto id = std::find(a.cbegin(), a.cend(), "1");
-    printf("id [%s] [0x%x]\n", id->c_str(), id); //find查找指定元素
-    auto num = std::count(a.cbegin(), a.cend(), "1"); //count返回个数
-    std::string tmp("join a: ");
-    std::string group = std::accumulate(a.cbegin(), a.cend(), tmp); // 求和函数
-    printf("num [%d]\n", num); //find查找指定元素
+    auto id = std::find(svec.cbegin(), svec.cend(), "11");
+    printf("id [%s] [0x%x]\n", id->c_str(), id);             // find查找指定元素
+    auto num = std::count(svec.cbegin(), svec.cend(), "11"); // count返回个数
+    std::string tmp("join svec: ");
+    std::string group = std::accumulate(svec.cbegin(), svec.cend(), tmp); // 求和函数
+    printf("num [%d]\n", num);                                            // find查找指定元素
     std::cout << "group " << group << std::endl;
+
+    std::fill_n(std::back_inserter(ivec), 3, 0);                                    // 插入元素
+    std::replace(ivec.begin(), ivec.end(), 0, 3);                                   // 把所有0替换为3
+    std::replace_copy(ivec.cbegin(), ivec.cend(), std::back_inserter(ivec2), 3, 6); // 不改变ivec， 将3替换为6， 放入ivec2
+    auto unique = std::unique(ivec2.begin(), ivec2.end());                          // 指向最后
+    ivec2.erase(unique, ivec2.end());
+
+    for (auto i : ivec2)
+    {
+        std::cout << i << " ";
+    }
+
+    // 向算法传递函数
+    std::sort(svec.begin(), svec.end(), IsShorter); // 自定义比较函数 IsShorter
+    for (auto i : svec)
+    {
+        std::cout << i << " ";
+    }
+
+    // 向算法传递lambda函数
+    std::sort(svec.begin(), svec.end(), [](const std::string &s1, const std::string &s2)
+              { return s1.size() > s2.size(); }); // 自定义lambda比较函数
+    for (auto i : svec)
+    {
+        std::cout << i << " ";
+    }
+
+    int biggest_length = 2;
+    // for_each 算法
+    std::for_each(std::find_if(svec.begin(), svec.end(), [biggest_length](const std::string &s)
+                               { return s.size() < biggest_length; }),
+                  svec.end(), [](const std::string &s)
+                  { std::cout << s << " "; }); //捕获局部变量biggest_length
+
+    std::cout << "end success" << std::endl;
     return 0;
 }
